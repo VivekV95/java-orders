3deb660c41ccd8af83136036e73fb6b4aa0f1034
VivekV95 <vivekv210@gmail.com>
2019-06-11 14:23:01 -0500

    Initial Commit

---
+++ src/test/kotlin/com/vivekvishwanath/javaorders/JavaordersApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.vivekvishwanath.javaorders
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class JavaordersApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
